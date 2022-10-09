70623444e192646789bcbedabf6d07165ef6ba54
GachiKing <108138471+GachiKing@users.noreply.github.com>
2022-10-09 20:55:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let button = document.getElementById("batn")
+let input = document.querySelector(".box") 
+
+function mathrandom() {
+    input.innerHTML = Math.floor(Math.random() * 300);
+}
+
+
+ 
+
+
